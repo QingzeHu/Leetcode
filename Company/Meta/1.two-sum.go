9064302b265d6fe0d136e043fcf5c6431f2ff8b1
Qingze Hu <qingzeinus@gmail.com>
2024-06-08 23:52:30 -0500

    chore: Add .gitignore file and implement twoSum function for LeetCode problem

---
+++ Company/Meta/1.two-sum.go
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=1 lang=golang
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+    result:=make(map[int]int)
+	for i, num := range nums{
+		complement:=target - num
+		if j, ok := result[complement];ok{
+			return []int{i, j}
+		}
+		result[num]=i
+	}
+	return nil
+}
+// @lc code=end
+
