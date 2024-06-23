cdd127799ca6e8da6f5e9a7a0d14c73ae3c9e9d3
Qingze Hu <qingzeinus@gmail.com>
2024-06-23 18:41:59 -0500

    day14

---
+++ website-questions/company/meta/314.binary-tree-vertical-order-traversal.go
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=314 lang=golang
+ *
+ * [314] Binary Tree Vertical Order Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func verticalOrder(root *TreeNode) [][]int {
+
+}
+
+// @lc code=end
+
